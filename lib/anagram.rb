9f999d82166274e3525820fd5b9d0d6a6eb79ed2
Charles Swaleh <charlswall@live.com>
2023-03-12 22:31:23 +0300

    Completed the anagram detector

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+
+require "pry"
+
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        matched = []
+        arr.each do |item|
+            if item.split("").sort == @word.split("").sort
+                matched.push(item)
+            end
+        end
+        return matched
+    end
+end
+
+
+# binding.pry
